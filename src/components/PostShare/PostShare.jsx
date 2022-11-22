0c765058c0fe4c0ba57264062580e9336e0f5715
Sulav Adhikari <sulavadhikari17@gmail.com>
2022-11-23 10:43:22 +1100

    image upload functionality done

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -1,38 +1,66 @@
-import React from 'react'
-import './postShare.css'
-import profileImage from '../../img/profileImg.jpg'
-import {UilScenery} from '@iconscout/react-unicons'
-import {UilPlayCircle} from '@iconscout/react-unicons'
-import {UilLocationPoint} from '@iconscout/react-unicons'
-import {UilSchedule} from '@iconscout/react-unicons'
-
+import React, { useState, useRef } from "react";
+import "./postShare.css";
+import profileImage from "../../img/profileImg.jpg";
+import { UilScenery } from "@iconscout/react-unicons";
+import { UilPlayCircle } from "@iconscout/react-unicons";
+import { UilLocationPoint } from "@iconscout/react-unicons";
+import { UilSchedule } from "@iconscout/react-unicons";
+import { UilTimes } from "@iconscout/react-unicons";
 
 export const PostShare = () => {
+  const [image, setImage] = useState(null);
+  const imageRef = useRef();
+  const onImageChange = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      let img = e.target.files[0];
+      setImage({
+        image: URL.createObjectURL(img),
+      });
+    }
+  };
   return (
     <div className="postShare">
-    <img src={profileImage} alt="" />
-    <div>
-      <input type='text' placeholder="What's happening" />
-      <div className="postoptions">
-    <div className="option">
-    <UilScenery />
-    Photo
+      <img src={profileImage} alt="" />
+      <div>
+        <input type="text" placeholder="What's happening" />
+        <div className="postoptions">
+          <div
+            className="option"
+            onClick={() => imageRef.current.click()}
+            style={{ color: "var(--photo)" }}
+          >
+            <UilScenery />
+            Photo
+          </div>
+          <div className="option" style={{ color: "var(--video)" }}>
+            <UilPlayCircle />
+            Video
+          </div>
+          <div className="option" style={{ color: "var(--location)" }}>
+            <UilLocationPoint />
+            Location
+          </div>
+          <div className="option" style={{ color: "var(--schedule)" }}>
+            <UilSchedule />
+            Schedule
+          </div>
+          <button className="button-all ps-button">Share</button>
+          <div style={{ display: "none" }}>
+            <input
+              type="file"
+              name="myImage"
+              ref={imageRef}
+              onChange={onImageChange}
+            />
+          </div>
+        </div>
+        {image && (
+          <div className="previewImage">
+            <UilTimes onClick={()=>setImage(null)}/>
+            <img src={image.image} alt='' />
+          </div>
+        )}
+      </div>
     </div>
-    <div className="option">
-    <UilPlayCircle />
-    Video
-    </div>
-    <div className="option">
-    <UilLocationPoint />
-    Location
-    </div>
-    <div className="option">
-    <UilSchedule />
-    Schedule
-    </div>
-    </div>
-    </div>
- 
-    </div>
-  )
-}
+  );
+};
--- src/components/PostSide/PostSide.jsx
+++ src/components/PostSide/PostSide.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Posts } from '../Posts/Posts'
 import { PostShare } from '../PostShare/PostShare'
 import './postSide.css'
 export const PostSide = () => {
   return (
     <div className='postSide'>
         <PostShare />
+        <Posts />
     </div>
   )
 }
